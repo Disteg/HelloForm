b96e84b2d9b99e4d30306fa26c4259dec57f4283
Disteg <stegantsovdima@yandex.ru>
2020-11-06 07:25:10 +0300

    initi commit

--- script/api.js
+++ script/api.js
@@ -1,5 +1,6 @@
 const ButtonOut = document.querySelector('.header__button_button');
 const popup = document.querySelector('.popup');
+const btnclose = document.querySelector('.popup__btn-close')
 
 
 function openPopup () {
@@ -10,11 +11,11 @@ function colorbutton(){
     ButtonOut.classList.toggle('color_btn')
 }
     
-function closePopup(popup){
+function closePopup(){
 popup.classList.remove('popup_open')
 }
 
 
-
+btnclose.addEventListener('click',closePopup )
 ButtonOut.addEventListener('click', openPopup);
 ButtonOut.addEventListener('click', colorbutton);

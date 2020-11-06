4b863344926f3db6a6de0060408c090659daed54
Disteg <stegantsovdima@yandex.ru>
2020-11-06 09:16:24 +0300

    initi commit

--- script/api.js
+++ script/api.js
@@ -1,6 +1,10 @@
 const ButtonOut = document.querySelector('.header__button_button');
 const popup = document.querySelector('.popup');
-const btnclose = document.querySelector('.popup__btn-close')
+const btnclose = document.querySelector('.popup__btn-close');
+const inputPassword = document.querySelector('#input-password');
+const inputLogo = document.querySelector('#input-logo');
+const btnPopup = document.querySelector('.btnPopup');
+
 
 
 function openPopup () {
@@ -16,6 +20,7 @@ popup.classList.remove('popup_open')
 }
 
 
+btnclose.addEventListener('click', colorbutton)
 btnclose.addEventListener('click',closePopup )
 ButtonOut.addEventListener('click', openPopup);
 ButtonOut.addEventListener('click', colorbutton);

dc03d5bec96678f507b89bd3253e262eac5b60b5
Disteg <stegantsovdima@yandex.ru>
2020-11-06 06:58:12 +0300

    initi commit

--- script/api.js
+++ script/api.js
@@ -1,8 +1,17 @@
 const ButtonOut = document.querySelector('.header__button_button');
 const popup = document.querySelector('.popup');
 
+
 function openPopup () {
-    popup.classList.add('popup_open')
+    popup.classList.add('popup_open');
 };
 
+function colorbutton(){
+    ButtonOut.classList.toggle('color_btn')
+}
+    
+
+
+
 ButtonOut.addEventListener('click', openPopup);
+ButtonOut.addEventListener('click', colorbutton);
